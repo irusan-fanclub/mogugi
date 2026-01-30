96af02f48cae2ae48a9a48f7670056e3925fd9be
LiYu <mickey9910326@gmail.com>
2026-01-30 15:56:16 +0800

    Change serverip support tw and gearup

--- constants/const.go
+++ constants/const.go
@@ -2,22 +2,29 @@ package constants
 
 import (
 	"fmt"
-	"strings"
 	"time"
 )
 
 var PCAP_GAMESERVER_FILTER = ""
 
-// kr 기준
-const _GAMESERVER_NET = "211.218.233.0/24"
+// kr server
+// const _SERVER_MABI_KR = "211.218.233.0/24"
+// const _PORT_MABI_KR = "11020 or 11021 or 11023"
 
-var _GAMESERVER_PORT = []string{"11020", "11021", "11023"}
+// tw server
+const _SERVER_MABI_TW = "210.208.80.0/24"
+const _PORT_MABI_TW = "11022 or 59062"
+
+// Gearup Taiwan server (Taiwan 15)
+const _SERVER_GEARUP = "43.212.170.59"
+const _PORT_GEARUP = "2082"
 
 func init() {
-	filter := fmt.Sprint("tcp and src net ", _GAMESERVER_NET)
-	filter += fmt.Sprint(" and src port ( ", strings.Join(_GAMESERVER_PORT, " or "), " ) ")
+	// Filter for Mabi server or Gearup server
+	filterMabi := fmt.Sprintf("(tcp and src net %s and src port (%s))", _SERVER_MABI_TW, _PORT_MABI_TW)
+	filterGearup := fmt.Sprintf("(tcp and src net %s and src port (%s))", _SERVER_GEARUP, _PORT_GEARUP)
 
-	PCAP_GAMESERVER_FILTER = filter
+	PCAP_GAMESERVER_FILTER = fmt.Sprintf("%s or %s", filterMabi, filterGearup)
 }
 
 var SERVER_START_AT = time.Now().Unix()
